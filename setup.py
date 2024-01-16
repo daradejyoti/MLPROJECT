bf7c7780a422cb3e329800a4c1b8e2431a3ed787
ParagDD <paragdarade234@gmail.com>
2024-01-16 14:40:29 +0530

    5th commit

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import find_packages,setup
 
 def get_requirements(file_path:str)->list[str]:
     requirements =[]
-    with open(file_path ,'r') as file:
+    with open(file_path) as file:
         requirements = file.readlines()
-        requirements=[i.replace("/n",'') for i in requirements ]
+        requirements=[i.replace("/n",'') for i in requirements]
+    
     return requirements
 
 
@@ -15,12 +16,9 @@ def get_requirements(file_path:str)->list[str]:
 
 setup(
     name= "ml_project",
-    version= '0.1',
-    author= "jyoti",
+    version= '0.1.1',
+    author= "j",
     author_email= 'daradejyoti21@gmail.com',
-    packages = find_packages,
-    requires= get_requirements('requirements.txt')
-  
-
-
-)
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+  )
